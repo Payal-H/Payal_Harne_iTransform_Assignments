b3b3450216989344f82d4513db077e47d7ce395e
Payal-H <harnepayal262@gmail.com>
2021-04-13 23:20:27 +0530

    Adding Files inside Directory Assignments

---
+++ Assignments/Hello.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+  <p>Edited Part in eclipse</p>
+
+</body>
+
+</html>
---
+++ Assignments/README.txt
@@ -0,0 +1,8 @@
+Changes According to section 3 Q 8
+This is readme file for Git Branching Assignment
+changes section 2 Q 8
+changes in readme again
+Pushing files using eclipse
+Push 2
+trying to merge
+branch fix bug
